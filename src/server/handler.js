f7c1582520c8ed04adaa642d843acc2e1e9011d0
muhammad-asyam <muhammadasyam701@gmail.com>
2024-05-23 13:20:44 +0700

    Push code to remote repository

--- src/server/handler.js
+++ src/server/handler.js
@@ -20,11 +20,11 @@ async function postPredictHandler(request, h) {
   }
   
  
-  const response = h.response({
-    status: 'success',
-    message: confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
-    data
-  })
+  // const response = h.response({
+  //   status: 'success',
+  //   message: confidenceScore > 99 ? 'Model is predicted successfully.' : 'Model is predicted successfully but under threshold. Please use the correct picture',
+  //   data
+  // })
   response.code(201);
   return response;
 }
